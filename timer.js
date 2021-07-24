5de52c7dde95c79f9c1ea22368147c6d11e00d69
Sebastian Anhofer <sebastian.anhofer@gmail.com>
2021-07-24 23:21:43 +0200

    use vue maybe

--- timer.js
+++ timer.js
@@ -1,3 +1,198 @@
+let app = new Vue({
+	el: '#app',
+	data: {
+		updateTitle: true,
+		input: "",
+	},
+	computed: {
+		data: {
+			get: function (){
+				let cookie = {};
+				try {
+					cookie = JSON.parse(document.cookie);
+				} catch (error) {}
+
+				if (cookie.vol == undefined){
+					cookie.vol = 10;
+				}
+				if (cookie.alarms == undefined){
+					cookie.alarms = [];
+				}
+				return cookie;
+			},
+			set: function (d){
+				console.log("writing cookie");
+				let expDate = new Date();
+				expDate.setFullYear(expDate.getFullYear() + 100);
+				document.cookie = JSON.stringify(d) + ";expires=" + expDate.toUTCString() + ";SameSite=Strict";
+			},
+		},
+	},
+	methods: {
+		getTimerString: function(time) {
+			let ctdMs = new Date(time) - new Date();
+
+			let h = Math.floor(ctdMs / 3600000);
+			ctdMs = ctdMs % 3600000;
+
+			let m = Math.floor(ctdMs / 60000);
+			ctdMs = ctdMs % 60000;
+
+			let s = Math.floor(ctdMs / 1000);
+
+			let timeStr = "";
+			let cut = true;
+			if (h != 0) {
+				timeStr += h;
+				timeStr += ":";
+				cut = false;
+			}
+			if (m != 0 || !cut) {
+				if (!cut && m < 10) {
+					timeStr += "0";
+				}
+				timeStr += m;
+				timeStr += ":";
+				cut = false;
+			}
+			if (!cut && s < 10) {
+				timeStr += "0";
+			}
+			timeStr += s;
+
+			if (this.updateTitle && timeStr != "0") {
+				document.title = timeStr;
+				this.updateTitle = false;
+			}
+
+			return timeStr;
+		},
+		createTimer: function(){
+			let input = this.input;
+			let alarmtime = new Date();
+			let validinput = false;
+
+			let hrcdt = /\d+\s*(?:hrs|hr|h|std)/i;
+			let mincdt = /\d+\s*(?:min|m|mins)/i;
+			let seccdt = /\d+\s*(?:s|sec|sek)/i;
+
+			let date =  /@\s*(\d\d?)(?:\.(\d\d?)(?:\.(\d\d\d\d+))?)?/i;
+
+			let alarm = /\d?\d:\d\d(?::\d\d)?/i;
+
+			let volume = /(?:volume|vol):?\s*(\d+)/i;
+
+			let comment = /\#(.*)/i;
+
+			let h = input.match(hrcdt);
+			let m = input.match(mincdt);
+			let s = input.match(seccdt);
+
+			let d = input.match(date);
+
+			let t = input.match(alarm);
+
+			let v = input.match(volume);
+
+			let c = input.match(comment);
+
+			if (input == "ring") {
+				this.soundAlarm();
+			} else if (v) {
+				v = v[1];
+				if (v < 0) {
+					v = 0;
+				} else if (v > 100) {
+					v = 100;
+				}
+				this.data.vol = v;
+			} else if (h || m || s) {
+				validinput = true;
+				if (h) {
+					h = extractNumbers(h[0]);
+					h = h * 3600000;
+					addTime(alarmtime, h);
+				}
+				if (m) {
+					m = extractNumbers(m[0]);
+					m = m * 60000;
+					addTime(alarmtime, m);
+				}
+				if (s) {
+					s = extractNumbers(s[0]);
+					s = s * 1000;
+					addTime(alarmtime, s);
+				}
+			} else if (t) {
+				validinput = true;
+
+				if (d) {
+					console.log(d);
+
+					let day = d[1];
+					let mon = d[2];
+					let year = d[3];
+
+					if (day) {
+						alarmtime.setDate(day);
+					}
+
+					if (mon) {
+						alarmtime.setMonth(mon - 1);
+					}
+
+					if (year) {
+						alarmtime.setFullYear(year);
+					}
+				}
+
+				t = t[0].split(":");
+				if (t[0] >= 0 && t[0] <= 24) {
+					alarmtime.setHours(t[0]);
+				} else {
+					validinput = false;
+				}
+				if (t[1] >= 0 && t[1] <= 59) {
+					alarmtime.setMinutes(t[1]);
+				} else {
+					validinput = false;
+				}
+				if (t[2]) {
+					if (t[2] >= 0 && t[2] <= 59) {
+						alarmtime.setSeconds(t[2], 0);
+					} else {
+						validinput = false;
+					}
+				} else {
+					alarmtime.setSeconds(0, 0);
+				}
+
+				if (new Date() - alarmtime > 0) {
+					alarmtime.setDate(alarmtime.getDate() + 1);
+				}
+			}
+
+			this.input = "";
+			if (validinput) {
+				console.log(alarmtime);
+				this.addTimer(alarmtime.toISOString(), c?c[1]:"");
+			}
+		},
+		addTimer: function(time, comment){
+			let alarm = {time: time, comment: comment};
+			this.data.alarms.push(alarm);
+		},
+		soundAlarm: function(){
+			let audio = this.$refs.ring;
+			if (audio.paused) {
+				audio.play();
+				return true;
+			}
+			return false;
+		},
+	},
+});
+
 function createTimer() {
 	let input = $("#inputbox")[0].value;
 	let alarmtime = new Date();
@@ -318,14 +513,14 @@ function setCookieData(data) {
 	document.cookie = JSON.stringify(data) + ";expires=" + expDate.toUTCString() + ";SameSite=Strict";
 }
 
-$(document).ready(function () {
-	$("#inputform").on("submit", createTimer);
+// $(document).ready(function () {
+// 	$("#inputform").on("submit", createTimer);
 
-	let vol = getVolume();
-	setVolume(vol);
+// 	let vol = getVolume();
+// 	setVolume(vol);
 
-	restoreTimers();
-	updateCountdowns();
+// 	restoreTimers();
+// 	updateCountdowns();
 
-	window.setInterval(updateCountdowns, 1000);
-});
+// 	window.setInterval(updateCountdowns, 1000);
+// });
